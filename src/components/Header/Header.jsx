5025945759463a82e059a5c8bde05f5a4b4dd527
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-10-13 20:00:16 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,5 @@
-import { Link, Outlet } from "react-router-dom";
 
 export const Header = () => {
-    return (
-      <>
-        {/* <h1>MovieDetails:</h1>
-        <ul>
-          <li>
-            <Link to="cast">Cast</Link>
-          </li>
-          <li>
-            <Link to="review">Review</Link>
-          </li>
-        </ul>
-        <Outlet />
-      </> */}
-    );
+    return 
+      
 }
